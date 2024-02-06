dda68177132a9c5a7c01c525b5b484aca95b529f
mdifal <mdifa1565@gmail.com>
2024-02-06 23:16:47 +0700

    Push differen url for different ways of testing

--- flutter_hello_world/lib/main.dart
+++ flutter_hello_world/lib/main.dart
@@ -18,18 +18,32 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   String message = '';
 
-  // Future<void> fetchMessage() async {
-  //   final http.Response response =
-  //       await http.get(Uri.parse('http://localhost:8000/api/hello'));
   Future<void> fetchMessage() async {
-    final http.Response response =
-        await http.get(Uri.parse('http://10.0.2.2:8000/api/hello'));
-    if (response.statusCode == 200) {
-      final data = jsonDecode(response.body);
-      setState(() {
-        message = data['message'];
-      });
-    } else {
+    try {
+      final http.Response response =
+      //For Web Browser
+      await http.get(Uri.parse('http://localhost:8000/api/hello'));
+
+      //For VM
+      //await http.get(Uri.parse('http://10.0.2.2:8000/api/hello'));
+
+      //For Android
+      //await http.get(Uri.parse('http://(IP):8000/api/hello'));
+
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body);
+        setState(() {
+          message = data['message'];
+        });
+      } else {
+        // This part is for handling non-200 HTTP responses.
+        setState(() {
+          message = 'Failed to fetch message';
+        });
+      }
+    } catch (e) {
+      // This catch block handles exceptions thrown due to network issues,
+      // such as when the address is not found or the server is unreachable.
       setState(() {
         message = 'Failed to fetch message';
       });
